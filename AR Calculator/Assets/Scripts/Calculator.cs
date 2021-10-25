cf72f385d06a3202e1813f1de4354214a6711a4d
alistair-hudson <alistair.hudson64@gmail.com>
2021-10-26 01:01:22 +0300

    basic calculator consttucted with unity ui

--- AR Calculator/Assets/Scripts/Calculator.cs
+++ AR Calculator/Assets/Scripts/Calculator.cs
@@ -7,19 +7,96 @@ public class Calculator : MonoBehaviour
 {
     [SerializeField]TMP_Text display = null;
 
+    Queue<string> storedEquation = new Queue<string>();
+    
+
+    private void Start()
+    {
+        display.text = "";
+    }
 
     public void NumberInput(int number)
     {
         display.text += number.ToString();
     }
-  
-    public void SymbolButton(string symbol)
+
+    public void DecimalPoint()
     {
-        display.text += symbol;
+        if("" == display.text)
+        {
+            display.text = "0.";
+        }
+        else if (!display.text.Contains("."))
+        {
+            display.text += ".";
+        }
+    }
+  
+    public void Add()
+    {
+        storedEquation.Enqueue(display.text);
+        display.text = "";
+        storedEquation.Enqueue("+");
+    }
+
+    public void Multiply()
+    {
+        storedEquation.Enqueue(display.text);
+        display.text = "";
+        storedEquation.Enqueue("*");
+    }
+
+    public void Divide()
+    {
+        storedEquation.Enqueue(display.text);
+        display.text = "";
+        storedEquation.Enqueue("/");
+    }
+
+    public void Subtract()
+    {
+        if ("" == display.text)
+        {
+            display.text = "-";
+        }
+        else
+        {
+            storedEquation.Enqueue(display.text);
+            display.text = "";
+            storedEquation.Enqueue("-");
+        }
     }
 
     public void Equals()
     {
+        storedEquation.Enqueue(display.text);
+        float total = float.Parse(storedEquation.Dequeue());
+        while (0 < storedEquation.Count)
+        {
+            string str = storedEquation.Dequeue();
+            if ("+" == str)
+            {
+                total += float.Parse(storedEquation.Dequeue());
+            }
+            else if ("-" == str)
+            {
+                total -= float.Parse(storedEquation.Dequeue());
+            }
+            else if ("*" == str)
+            {
+                total *= float.Parse(storedEquation.Dequeue());
+            }
+            else if ("/" == str)
+            {
+                total /= float.Parse(storedEquation.Dequeue());
+            }
+        }
+        display.text = total.ToString();
+    }
 
+    public void Clear()
+    {
+        display.text = "";
+        storedEquation.Clear();
     }
 }
